8812e29d757225e6f67b3a6d2ffad00b5fe78062
Marcin-Blok <marcinblok7980@gmail.com>
2020-04-29 07:56:50 +0200

    Dodanie klasy OfficeDAO i zapis do bazy danych połączenia typu One to Many

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,14 +1,23 @@
 import java.util.ArrayList;
 import java.util.List;
 
+import pl.marcinblok.jpa.mysqlhibernatejava.dao.OfficeDAO;
 import pl.marcinblok.jpa.mysqlhibernatejava.dao.ProductDAO;
 import pl.marcinblok.jpa.mysqlhibernatejava.dao.ProductLineDAO;
+import pl.marcinblok.jpa.mysqlhibernatejava.entity.Office;
 import pl.marcinblok.jpa.mysqlhibernatejava.entity.Product;
 import pl.marcinblok.jpa.mysqlhibernatejava.entity.ProductLine;
 
 public class App {
 	public static void main(String[] args) {
 		ProductLine productLine = new ProductLine("buty2", "cos cos tam", "<p> cos tam opis</a>", null);
+		Office office = new Office("DEV-14", "Boston", "0136465734", "woiejfw", "doweijewijwo", "New Jersey", "USA", "057-NY", "owijf");
+		Office office2 = new Office("RM-12", "Praga", "475623970", "Rynek 6", "RUE89U", "NA" , "Czechy", "12-26-4", "EU");
+		
+		OfficeDAO officeDAO = new OfficeDAO();
+		officeDAO.add(office);
+		officeDAO.add(office2);
+		
 
 		ProductLineDAO productLineDAO = new ProductLineDAO();
 		productLineDAO.add(productLine);
